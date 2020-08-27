a3fb49e9236cb7a521c36e8e6f1a69076d56e9be
Derkian Souza <derkian@iands.com.br>
2020-08-27 17:11:54 -0300

    fix: Correção de caminho de arquivo

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { parseISO } from "date-fns";
-import AppointmentsRepository from "./repositories/AppointmentsRepository";
-import CreateAppointmentService from "./services/CreateAppointmentServices";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from "../services/CreateAppointmentServices";
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
